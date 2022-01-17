581df9468530ba6fc9d67e2ebc5390364ea8af93
Gerardo Zelaya Eufemia <menosgeze@gmail.com>
2022-01-16 22:51:34 -0500

    remove trailing spaces

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -1,3 +1,8 @@
+"""
+This script removes all trailing spaces of files with the given extensions
+in a git repository, and then commits this change.
+"""
+
 import argparse
 import os
 
@@ -34,6 +39,7 @@ def listing_files(dirname: str, extensions_list: list):
 
     return files_to_read
 
+
 def removing_spaces(list_of_files):
     """
     For each file in list_of_files, it removes the trailing spaces,
@@ -44,12 +50,12 @@ def removing_spaces(list_of_files):
     """
 
     for filename in list_of_files:
-         commands = []
-         commands.append(f"sed 's/[ \t]*$//' {filename} > {filename}_tmp")
-         commands.append(f"mv {filename}_tmp {filename}")
-         commands.append(f"git add {filename}")
-         for cmd in commands:
-             os.system(cmd)
+        commands = []
+        commands.append(f"sed 's/[ \t]*$//' {filename} > {filename}_tmp")
+        commands.append(f"mv {filename}_tmp {filename}")
+        commands.append(f"git add {filename}")
+        for cmd in commands:
+            os.system(cmd)
     os.system("git commit -m 'remove trailing spaces'")
 
 
@@ -60,19 +66,25 @@ def main():
     and then removes the trailing spaces from all of them, and
     """
 
-    # reading terminal arguments: repository path and list of extensions to consider.
+    # reading terminal arguments:
+    # repository path and list of extensions to consider.
     parser = argparse.ArgumentParser(
          description='')
     parser.add_argument(
         '--git_repo_root', '--rr', type=str, default=None,
         help='absolute path of the repository root')
     parser.add_argument(
-        '--extensions_list', '--el', type=list, default = ['py'],
+        '--extensions_list', '--el', type=list, default=['py'],
         help='list of extensions of files to modify')
     args = parser.parse_args()
 
+    # terminating if no repo path provided
     if args.git_repo_root is None:
         print("Please provide a valid repository path")
+
+
+    os.chdir(args.git_repo_root)
+    print(f"LOG: changing working directory to {args.git_repo_root}")
 
     # printing arguments for logs.
     print(f"LOG: repository root = {args.git_repo_root}")
@@ -83,19 +95,20 @@ def main():
         args.git_repo_root,
         extensions_list=args.extensions_list)
 
-    print(f"LOG: there are {len(files_to_read)} in this repository")
-    print(f"     e.g. ?{files_to_read[0]}?")
-
+    # terminating if there are no files as required.
     if len(files_to_read) == 0:
         print(
-            f"There are no files with "
+            "There are no files with "
             "the given extensions in this repository")
         exit()
+
+    print(f"LOG: there are {len(files_to_read)} in this repository")
 
     # removing trailing spaces
     removing_spaces(files_to_read)
 
     print("Now push these changes.")
+
 
 if __name__ == '__main__':
     main()
